5234206c4609b5de938e7542f1548867e4cabf4b
Fadi Haddad <haddad207@gmail.com>
2024-03-12 16:08:09 -0700

    Added Pagination/filtering to API and Client

---
+++ client/src/app/components/RadioButtonGroup.tsx
@@ -0,0 +1,38 @@
+import {
+  FormControl,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+} from "@mui/material";
+
+interface Props {
+  options: {
+    value: string;
+    label: string;
+  }[];
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  selectedValue: string;
+}
+
+export default function RadioButtonGroup({
+  options,
+  onChange,
+  selectedValue,
+}: Props) {
+  return (
+    <FormControl>
+      <RadioGroup onChange={onChange} value={selectedValue}>
+        {options.map(({ value, label }) => {
+          return (
+            <FormControlLabel
+              value={value}
+              control={<Radio />}
+              label={label}
+              key={value}
+            />
+          );
+        })}
+      </RadioGroup>
+    </FormControl>
+  );
+}
--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,17 +1,24 @@
 import { Grid } from "@mui/material";
 import { Product } from "../../app/models/product";
 import ProductCard from "./ProductCard";
+import { useAppSelector } from "../../app/store/configureStore";
+import ProductSkeleton from "./ProductSkeleton";
 
 interface Props {
   products: Product[];
 }
 
 function ProductList({ products }: Props) {
+  const { productsLoaded } = useAppSelector((state) => state.catalog);
   return (
     <Grid container spacing={4}>
       {products.map((product) => (
-        <Grid item xs={3} key={product.id}>
-          <ProductCard product={product} />
+        <Grid item xs={4} key={product.id}>
+          {!productsLoaded ? (
+            <ProductSkeleton />
+          ) : (
+            <ProductCard product={product} />
+          )}
         </Grid>
       ))}
     </Grid>
